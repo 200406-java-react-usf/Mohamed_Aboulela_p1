43fbcc3f7749ed11b7e63b0f73e34c3db640853f
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-12 19:36:41 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,43 @@
+import dotenv from 'dotenv';
 import express from 'express';
-let app = express();
+import fs from 'fs';
+// import morgan from 'morgan';
+const morgan = require('morgan');
+import path from 'path';
 
-const port = 8080;
+import { UserRouter } from './routers/user-router';
+import { sessionMiddleware } from './middleware/session-middleware';
+import { corsFilter } from './middleware/cors-filter';
+import { Pool } from 'pg';
 
-app.use('/', express.json());
-app.get('/', (req, res) => {
-	res.send('setup complete');
+
+// environment configuration
+dotenv.config();
+
+// database configuration
+export const connectionPool: Pool = new Pool({
+    host: process.env['DB_HOST'],
+    // port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
 });
 
+console.log(connectionPool);
+// logging configuration
+fs.mkdir(`${__dirname}/logs`, () => {});
+const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
 
-app.listen(port, () => {
-	console.log(`Applicatino is now running on port: ${port}`);
+// web server configuration
+const app = express();
+app.use(morgan('combined', { stream: logStream }));
+app.use(sessionMiddleware);
+app.use(corsFilter);
+app.use('/', express.json());
+app.use('/users', UserRouter);
+
+
+app.listen(8080, () => {
+    console.log(`Application running and listening at: http://localhost:8080`);
 });
