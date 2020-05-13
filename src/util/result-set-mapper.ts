d68d6cff3b4e0adfcf4baf488e2691452bfd00c1
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-12 19:36:31 -0500

    Update result-set-mapper.ts

---
+++ src/util/result-set-mapper.ts
@@ -0,0 +1,19 @@
+import { UserSchema } from "./schemas";
+import { User } from "../models/user";
+
+export function mapUserResultSet(resultSet: UserSchema): User {
+    
+    if (!resultSet) {
+        return {} as User;
+    }
+
+    return new User(
+        resultSet.ers_user_id,
+        resultSet.username,
+        resultSet.password,
+        resultSet.first_name,
+        resultSet.last_name,
+        resultSet.email,
+        resultSet.role
+    );
+}
