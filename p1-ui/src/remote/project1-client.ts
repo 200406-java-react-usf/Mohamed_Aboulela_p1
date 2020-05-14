1d9504ef716b3bb3f8963dc26d597397b789a5d9
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-14 10:35:25 -0500

    Update project1-client.ts

--- p1-ui/src/remote/project1-client.ts
+++ p1-ui/src/remote/project1-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const project1Client = axios.create({
-    baseURL: 'Project1Api-env.eba-mz7ndbyz.us-east-1.elasticbeanstalk.com',
+    baseURL: 'http://Project1Api-env.eba-mz7ndbyz.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
     }
